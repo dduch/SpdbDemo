0c3029219003c005358981a09c8f4468a5e7e2f5
BryQ47 <m.bryk@onet.pl>
2016-04-16 10:24:24 +0200

    Basic interfaces for navigation

---
+++ SpdbDemo/Navigation/Types.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpdbDemo.Navigation
+{
+    enum RouteType
+    {
+        All,
+        Cycle
+    }
+
+    public struct Point
+    {
+        double Lat;
+        double Long;
+    }
+
+    public interface IRoute
+    {
+        IEnumerable<Point> GetPoints();
+
+        double GetLength();
+
+        // This method will require reconsideration - is it really needed?
+        double GetCycleLenght();
+
+        IRoute Append(IRoute toAppend);
+    }
+
+    interface IGeoDataProvider
+    {
+        IRoute GetRoute(Point source, Point destination, RouteType prefferedType,  double lengthRestriction);
+
+        IEnumerable<Point> GetStations();
+    }
+
+    interface INetwork
+    {
+        IRoute GetBestRoute(Point source, Point destination);
+    }
+
+
+}
