801300d1b3048c6beb261bb402f69d374cf4afc2
Dawid Dominiak <dawid.dominiak@live.com>
2016-04-19 16:19:43 +0200

    Windsor resolver

---
+++ SpdbDemo/DI/WindsorDependecyResolver.cs
@@ -0,0 +1,36 @@
+﻿using Castle.MicroKernel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http.Dependencies;
+
+namespace SpdbDemo.DI
+{
+    internal class WindsorDependencyResolver : System.Web.Http.Dependencies.IDependencyResolver
+    {
+        private readonly IKernel container;
+
+        public WindsorDependencyResolver(IKernel container)
+        {
+            this.container = container;
+        }
+
+        public IDependencyScope BeginScope()
+        {
+            return new WindsorDependencyScope(this.container);
+        }
+
+        public object GetService(Type serviceType)
+        {
+            return this.container.HasComponent(serviceType) ? this.container.Resolve(serviceType) : null;
+        }
+
+        public IEnumerable<object> GetServices(Type serviceType)
+        {
+            return this.container.ResolveAll(serviceType).Cast<object>();
+        }
+
+        public void Dispose() { }
+    }
+}
---
+++ SpdbDemo/DI/WindsorDependencyScope.cs
@@ -0,0 +1,37 @@
+﻿using Castle.MicroKernel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http.Dependencies;
+using Castle.MicroKernel.Lifestyle;
+
+namespace SpdbDemo.DI
+{
+    public class WindsorDependencyScope : IDependencyScope
+    {
+        private readonly IKernel container;
+        private readonly IDisposable scope;
+
+        public WindsorDependencyScope(IKernel container)
+        {
+            this.container = container;
+            this.scope = container.BeginScope();
+        }
+
+        public object GetService(Type serviceType)
+        {
+            return this.container.HasComponent(serviceType) ? this.container.Resolve(serviceType) : null;
+        }
+
+        public IEnumerable<object> GetServices(Type serviceType)
+        {
+            return this.container.ResolveAll(serviceType).Cast<object>();
+        }
+
+        public void Dispose()
+        {
+            this.scope.Dispose();
+        }
+    }
+}
