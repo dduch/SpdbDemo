714217803ac3382bd087e9715e6ab6f66473ea67
BryQ47 <m.bryk@onet.pl>
2016-04-20 23:38:24 +0200

    Completed optimistic subgraphs construction.

--- NavigationResolver/DataModels/Network.cs
+++ NavigationResolver/DataModels/Network.cs
@@ -14,88 +14,23 @@ namespace NavigationResolver.DataModels
         private static object syncRoot = new Object();
 
         private SuperGraph graph;
+        private IGeoDataProvider geoData;
+        private TravelMetric metric;
 
-        private Network(IGeoDataProvider geoData, TravelMetric metric)
+        private Network(SuperGraph graph, IGeoDataProvider geoData, TravelMetric metric)
         {
-
-            var stationsToCheck = geoData.GetStations().ToList();
-            var subGraphs = new List<SubGraph>();
-            var maxFreeDist = metric.FreeOfChargeTime * metric.Velocity;
-
-            // 1) Build subgraphs of with free travel cost
-
-            while (stationsToCheck.Count > 0)
-            {
-                // Variables initialization:
-                var v = stationsToCheck.First();
-                var G = new SubGraph(metric.Velocity, metric.ChangeTime);
-                stationsToCheck.RemoveAt(0);
-                // Q contains vertices of G, for which distance to other vertices needs to be evaluated
-                // Stored information is: 
-                // 1. index of v in G vertices list
-                // 2. current index in stationsToCheck
-                var Q = new Queue<Point>();
-                Q.Enqueue(v);
-                
-                // This loop is work in progress
-                // It has many issues and does not work for now
-                // TODO: Finish it
-                while(Q.Count > 0)
-                {
-                    v = Q.Dequeue();
-
-                    var vertex = new Vertex(v);
-
-                    for (int i = 0; i < stationsToCheck.Count; ++i)
-                    {
-                        var u = stationsToCheck[i];
-                        var directDistance = v.GetDistanceTo(u);
-                        if (directDistance > maxFreeDist)
-                            continue;
-
-                        var arch = geoData.GetRoute(v, u, RouteType.Cycle, maxFreeDist);
-                        if (arch.GetLength() > maxFreeDist)
-                            continue; // TODO: We will probably be searching for this route once more if geoData has no cash. Needs to rewrite this later.
-
-                        var revArch = geoData.GetRoute(u, v, RouteType.Cycle, maxFreeDist);
-                        if (revArch.GetLength() < maxFreeDist)
-                        {
-                            // u and v are both in G for sure
-                            vertex.Neighbors.Add(G.Vertices.Count + i + 1);
-                            vertex.Archs.Add(arch);
-                        }
-                        else
-                        {
-                            // Problematic situation:
-                            // u may belong to G, but also may not
-                            // TODO: Add handling such cases
-                        }
-
-                        G.Vertices.Add(vertex);
-                    }
-
-                }
-
-                subGraphs.Add(G);
-
-            }
-
-            // 2) Build connections between subgraphs
-
-            // foreach g,h in G 
-            //      e = create_edge_of_lowest_cost(g, h);
-            //      g.NeighborGraphs.Add(h);
-            //      g.IntergraphArchs.Add(e);
-
-            graph = new SuperGraph(subGraphs, metric);
-
-            
+            this.graph = graph;
+            this.geoData = geoData;
+            this.metric = metric;       
         }
 
         // Creates new instance of network with given parameters.
         public static INetwork Build(IGeoDataProvider geoData, TravelMetric metric)
         {
-            return new Network(geoData, metric);
+            var builder = new NetworkBuilder(geoData, metric);
+            var subGraphs = builder.BuildSubGraphs();
+            var superGraph = new SuperGraph(subGraphs, metric);
+            return new Network(superGraph, geoData, metric);
         }
 
         // Gets existing instance of network
--- NavigationTest/NetworkTests.cs
+++ NavigationTest/NetworkTests.cs
@@ -15,11 +15,64 @@ namespace NavigationTest
         private FakeGeoDataProvider fakeGeoData = new FakeGeoDataProvider();
 
         [Test]
-        public void BuildGraph()
+        public void BuildSubGraph()
         {
-            var stations = fakeGeoData.GetStations().ToList();
+            double baseVelocity = 1.666;
+            TravelMetric testMetric;
+            NetworkBuilder builder;
+            List<SubGraph> graphs;
 
-            var dist = stations[12].GetDistanceTo(stations[17]);
+            // ---------- Test case 1 ----------
+
+            testMetric = new TravelMetric(baseVelocity, 100.0);
+            builder = new NetworkBuilder(fakeGeoData, testMetric);
+            graphs = builder.BuildSubGraphs();
+
+            Assert.AreEqual(3, graphs.Count);
+            Assert.IsTrue(graphs.Exists(g => g.Vertices.Count == 12));
+            Assert.IsTrue(graphs.Exists(g => g.Vertices.Count == 6));
+            Assert.IsTrue(graphs.Exists(g => g.Vertices.Count == 5));
+
+            // ---------- Test case 2 ----------
+
+            testMetric = new TravelMetric(baseVelocity * 1.5, 100.0);
+            builder = new NetworkBuilder(fakeGeoData, testMetric);
+            graphs = builder.BuildSubGraphs();
+
+            Assert.AreEqual(3, graphs.Count);
+            Assert.IsTrue(graphs.Exists(g => g.Vertices.Count == 12));
+            Assert.IsTrue(graphs.Exists(g => g.Vertices.Count == 6));
+            Assert.IsTrue(graphs.Exists(g => g.Vertices.Count == 5));
+
+            // ---------- Test case 3 ----------
+
+            testMetric = new TravelMetric(baseVelocity * 2.0, 100.0);
+            builder = new NetworkBuilder(fakeGeoData, testMetric);
+            graphs = builder.BuildSubGraphs();
+
+            Assert.AreEqual(2, graphs.Count);
+            Assert.IsTrue(graphs.Exists(g => g.Vertices.Count == 18));
+            Assert.IsTrue(graphs.Exists(g => g.Vertices.Count == 5));
+
+            // ---------- Test case 4 ----------
+
+            testMetric = new TravelMetric(baseVelocity * 4.0, 100.0);
+            builder = new NetworkBuilder(fakeGeoData, testMetric);
+            graphs = builder.BuildSubGraphs();
+
+            Assert.AreEqual(1, graphs.Count);
+            Assert.IsTrue(graphs.Exists(g => g.Vertices.Count == 23));
+
+            // ---------- Test case 4 ----------
+
+            testMetric = new TravelMetric(baseVelocity * 0.375, 100.0);
+            builder = new NetworkBuilder(fakeGeoData, testMetric);
+            graphs = builder.BuildSubGraphs();
+
+            Assert.AreEqual(20, graphs.Count);
+            Assert.IsTrue(graphs.FindAll(g => g.Vertices.Count == 1).Count == 18);
+            Assert.IsTrue(graphs.Exists(g => g.Vertices.Count == 2));
+            Assert.IsTrue(graphs.Exists(g => g.Vertices.Count == 3));
         }
     }
 }
